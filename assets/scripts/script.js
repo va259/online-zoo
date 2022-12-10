4a53050d2d445925362b5b98d1af94fb4cd363ca
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-12-10 15:43:22 +0100

    fix: testimonials - rework html layout

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,15 @@
 import { carouselInit } from './pets-carousel.mjs';
 import { toggleHandler } from './toggle-nav.mjs';
+import { scrollTestimonials } from './testimonials-scroll.mjs';
 
 const toggleNav = document.querySelector('.toggleNav');
+const range = document.querySelector('input[type="range"]');
 
 const init = () => {
+  alert(`Hi there! Please check back at the latest you can. Sorry, I'm a bit behind with the tasks, but trying to do my best. Thank you!`)
   toggleNav.addEventListener('click', toggleHandler);
   carouselInit();
-  // alert(`Hi there! Please check back at the latest you can. Sorry, I'm a bit behind with the tasks, but doing my best. Thank you!`)
+  range.addEventListener('click', scrollTestimonials);
 }
 
 document.addEventListener('DOMContentLoaded', init);
