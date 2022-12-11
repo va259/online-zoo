2b75d8f8ab9e1b57269956eb2d2a2d029e3bc18b
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-12-11 21:03:11 +0100

    fix: testimonails carousel refactor

--- assets/scripts/testimonials-carousel.mjs
+++ assets/scripts/testimonials-carousel.mjs
@@ -1,17 +1,44 @@
-const range = document.querySelector('input[type="range"]');
-const cardsWrapper = document.querySelector('.testimonials__inner');
-const card = document.querySelector('.testimonials .card');
+import { reviewsList } from '../data/testimonials-list.mjs'
 
-const cardWidth = card.offsetWidth;
-const containerGap = card.nextElementSibling.offsetLeft - cardWidth;
+const testimonials = document.querySelector('.testimonials')
+const range = document.querySelector('input[type="range"]');
+
+const renderCards = cards => {
+  const slide = document.createElement('div')
+  slide.classList.add('testimonials__inner')
+  
+  cards.reduce((_, card) => {
+    slide.innerHTML += `
+      <div class="card">
+        <div class="card_inner">
+            <div class="user">
+                <div class="user_icon ${card.imgClass}"></div>
+                <div class="user_info">
+                    <div class="name">${card.name}</div>
+                    <div class="local">${card.local}</div>
+                </div>
+            </div>
+            <p>${card.review}</p>
+        </div>
+      </div>
+    `
+  }, 0)
+  return slide;
+}
 
 const carouselHandler = event => {
+  const card = document.querySelector('.testimonials .card');
+  const cardsWrapper = document.querySelector('.testimonials__inner');
+  const cardWidth = card.offsetWidth;
+  const containerGap = card.nextElementSibling.offsetLeft - cardWidth;
+
   const value = range.value;
-  console.log(value);
   const length = cardWidth + containerGap;
+
   cardsWrapper.style.transform = `translateX(-${value * length}px)`;
 }
 
 export const testimonialsCarouselInit = () => {
+  testimonials.appendChild(renderCards(reviewsList));
   range.addEventListener('input', carouselHandler);
 }
